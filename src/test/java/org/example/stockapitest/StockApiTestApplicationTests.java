6e73c5f89e2c75147d37b0d8f58af0459e7ab76e
hyeonhee <gitgusgml042933@gmail.com>
2024-07-15 11:50:31 +0900

    init

---
+++ src/test/java/org/example/stockapitest/StockApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.stockapitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockApiTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
