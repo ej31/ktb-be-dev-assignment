74f7d6fe9a2cb5f19ffa14e445f67c451087f1d7
hyeonhee <gitgusgml042933@gmail.com>
2024-07-16 23:30:50 +0900

    fix.리드미 수정 및 불필요한 코드 삭제

--- src/main/java/org/example/stockapitest/security/ApiKeyFilter.java
+++ src/main/java/org/example/stockapitest/security/ApiKeyFilter.java
@@ -10,7 +10,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.example.stockapitest.exception.ApiKeyException;
 import org.example.stockapitest.response.CommonResponse;
-import org.example.stockapitest.service.ThrottleService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -21,8 +21,8 @@ public class ApiKeyFilter implements Filter {
     private static final String API_KEY_HEADER = "x-api-key";
     private static final String API_KEY = "c18aa07f-f005-4c2f-b6db-dff8294e6b5e";
 
-    @Autowired
-    private ThrottleService throttleService;
+//    @Autowired
+//    private ThrottleService throttleService;
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
@@ -32,12 +32,12 @@ public class ApiKeyFilter implements Filter {
         try {
             String apiKey = httpRequest.getHeader(API_KEY_HEADER); //요청 헤더에서 api키 가져오기
 
-            if (!throttleService.isRequestAllowed(apiKey)) {
-                throw new ApiKeyException(429, "Too Many Requests");
-            }
+//            if (!throttleService.isRequestAllowed(apiKey)) {
+//                throw new ApiKeyException(429, "Too Many Requests");
+//            }
 
             if (apiKey == null) {
-                throw new ApiKeyException(HttpServletResponse.SC_BAD_REQUEST, "No API key"); //
+                throw new ApiKeyException(HttpServletResponse.SC_BAD_REQUEST, "No API key");
             }
 
             if (!API_KEY.equals(apiKey)) {
