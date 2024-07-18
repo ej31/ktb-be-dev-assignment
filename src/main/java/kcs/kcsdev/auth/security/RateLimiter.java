bb1b5f02167c42192d6093239fac41062ff02284
Namgyu11 <hagaji3@naver.com>
2024-07-19 03:42:14 +0900

    feat: API 요청 허용률 제한 및 API 키 요청 관련 로직 추가
    - API 키 요청에 대한 허용률을 제한하는 로직을 추가.
    - 대량의 요청이 발생할 경우, 응답 코드 429를 반환.

--- src/main/java/kcs/kcsdev/auth/security/RateLimiter.java
+++ src/main/java/kcs/kcsdev/auth/security/RateLimiter.java
@@ -9,6 +9,12 @@ public class RateLimiter {
 
       private final ConcurrentHashMap<String, ConcurrentLinkedQueue<Long>> requestLogs = new ConcurrentHashMap<>();
 
+      /**
+       * 이 메서드는 주어진 API 키(apiKey)에 대한 요청이 허용되는지를 판단.
+       *
+       * @param apiKey 요청을 보낸 클라이언트의 API 키
+       * @return 요청이 허용되면 true, 그렇지 않으면 false 를 반환
+       */
       public boolean isRequestAllowed(String apiKey) {
             long currentTimeMillis = System.currentTimeMillis();
             requestLogs.putIfAbsent(apiKey, new ConcurrentLinkedQueue<>());
@@ -16,18 +22,22 @@ public class RateLimiter {
             ConcurrentLinkedQueue<Long> requestTimestamps = requestLogs.get(apiKey);
 
             synchronized (requestTimestamps) {
-                  // 10 seconds
-                  long timeWindowMillis = 10000;
+                  // 10초 시간 창
+                  long timeWindowMillis = 10000; // 10,000 밀리초 = 10초
+                  // 현재 시간에서 10초 이전의 요청 타임스탬프를 제거
                   while (!requestTimestamps.isEmpty()
                           && currentTimeMillis - requestTimestamps.peek() > timeWindowMillis) {
                         requestTimestamps.poll();
                   }
 
+                  // 최대 요청 수
                   int maxRequests = 10;
+                  // 현재 시간 창 내 요청 수가 최대 요청 수보다 적으면 요청을 허용
                   if (requestTimestamps.size() < maxRequests) {
                         requestTimestamps.add(currentTimeMillis);
                         return true;
                   } else {
+                        // 요청이 최대 요청 수를 초과하면 요청을 거부
                         return false;
                   }
             }
