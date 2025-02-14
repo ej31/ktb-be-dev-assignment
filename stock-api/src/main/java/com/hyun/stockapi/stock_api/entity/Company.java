a9d989aae60700eb2d739967d0bcbb8776b40568
hyun.lee <qwe7105c@naver.com>
2025-02-14 21:23:23 +0900

    feat : company 엔티티 추가

---
+++ stock-api/src/main/java/com/hyun/stockapi/stock_api/entity/Company.java
@@ -0,0 +1,21 @@
+package com.hyun.stockapi.stock_api.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "company")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Company {
+
+    @Id
+    @Column(name ="company_code", nullable = false, unique = true)
+    private String companyCode;
+
+    @Column(name ="company_name", nullable= false)
+    private String companyName;
+
+}
