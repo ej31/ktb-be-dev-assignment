6bf20a81bfbc4a0dac785fdee171363464ac99f9
hyoon1129 <hyoon1129@naver.com>
2025-02-15 02:38:22 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/ktb/yuni/YuniApplication.java
@@ -0,0 +1,13 @@
+package com.ktb.yuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ktb/yuni/YuniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktb.yuni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
