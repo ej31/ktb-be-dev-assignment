bda0cb334d9d7951caf295585fcdd8d14fd7daed
hyun.lee <qwe7105c@naver.com>
2025-02-14 22:12:42 +0900

    feat : company, stockhistory 레포지토러 추가

---
+++ stock-api/src/main/java/com/hyun/stockapi/stock_api/repository/StocksHistoryRepository.java
@@ -0,0 +1,19 @@
+package com.hyun.stockapi.stock_api.repository;
+
+import com.hyun.stockapi.stock_api.entity.StocksHistory;
+import com.hyun.stockapi.stock_api.entity.StocksHistoryId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Locale;
+
+@Repository
+public interface StocksHistoryRepository extends JpaRepository<StocksHistory, StocksHistoryId> {
+
+    List<StocksHistory> findByCompanyCodeAndTradeDateBetween(
+            String companyCode,
+            Locale startDate,
+            Locale endDate
+    );
+}
