0958e24fc24a17692a9bcf11ee54bf56df4d1757
pmsu2007 <pmsu2007@naver.com>
2025-02-14 20:04:13 +0900

    feat : Spring Boot Initializer

---
+++ src/test/java/ktb/mason/be_assignment/BeAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ktb.mason.be_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
