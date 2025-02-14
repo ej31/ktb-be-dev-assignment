c9a0c9bf0f1ae54d798006744cf5a7dc4c5ee8bb
hyun.lee <qwe7105c@naver.com>
2025-02-14 21:51:13 +0900

    fix : company 엔티티 수정

--- stock-api/src/main/java/com/hyun/stockapi/stock_api/entity/Company.java
+++ stock-api/src/main/java/com/hyun/stockapi/stock_api/entity/Company.java
@@ -2,24 +2,25 @@ package com.hyun.stockapi.stock_api.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+
 
 
 @Entity
 @Table(name = "company")
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Company {
 
     @Id
-    @Column(name ="company_code", nullable = false, unique = true)
+    @Column(name = "company_code", length = 10, nullable = false)
     private String companyCode;
 
-    @Column(name ="company_name", nullable= false)
+    @Column(name = "company_name", length = 100, nullable = false)
     private String companyName;
 
 }
