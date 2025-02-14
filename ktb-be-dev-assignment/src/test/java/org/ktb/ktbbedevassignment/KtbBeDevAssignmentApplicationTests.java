8ef71fe950dafd822b60c4959fcea323cf37611a
Unagi-zoso <Unagi.zoso@gmail.com>
2025-02-14 23:51:03 +0900

    Chore 프로젝트 생성

---
+++ ktb-be-dev-assignment/src/test/java/org/ktb/ktbbedevassignment/KtbBeDevAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ktb.ktbbedevassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtbBeDevAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
