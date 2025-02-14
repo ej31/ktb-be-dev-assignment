69a64e7c49e7b4fc221bc0f68066ae84a41948e5
sanghyuk Kim <tkdgur5031@naver.com>
2025-02-15 04:51:47 +0900

    feat: WebConfig.java Cors 설정 추가

--- src/main/java/org/ktb/dev/assignment/common/config/WebConfig.java
+++ src/main/java/org/ktb/dev/assignment/common/config/WebConfig.java
@@ -1,4 +1,22 @@
 package org.ktb.dev.assignment.common.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/*
+ * 프로트엔드 포트에 맞게 CORS 설정 수정 필요 !
+ * author : hyuk.kim (김상혁)
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
