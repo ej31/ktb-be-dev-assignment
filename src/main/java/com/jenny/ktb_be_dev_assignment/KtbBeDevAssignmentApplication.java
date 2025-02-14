628215a2e1590be188f194620317048f2468e5b9
jenny.kang <deco1202@naver.com>
2025-02-14 23:26:24 +0900

    Init Project

---
+++ src/main/java/com/jenny/ktb_be_dev_assignment/KtbBeDevAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.jenny.ktb_be_dev_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtbBeDevAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtbBeDevAssignmentApplication.class, args);
+	}
+
+}
