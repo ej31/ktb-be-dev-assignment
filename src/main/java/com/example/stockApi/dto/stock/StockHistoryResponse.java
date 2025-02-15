4d6842bc33183eddaf2ff24fa2ee553c3f303d18
choiyoorim <yulim3839@naver.com>
2025-02-16 01:56:26 +0900

    feat: ✨ 주식 정보 반환용 DTO 추가

--- src/main/java/com/example/stockApi/dto/stock/StockHistoryResponse.java
+++ src/main/java/com/example/stockApi/dto/stock/StockHistoryResponse.java
@@ -1,4 +1,16 @@
 package com.example.stockApi.dto.stock;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class StockHistoryResponse {
+    private String companyName;   // 기업명
+    private LocalDate tradeDate;  // 거래 날짜
+    private double closingPrice;  // 종가
 }
