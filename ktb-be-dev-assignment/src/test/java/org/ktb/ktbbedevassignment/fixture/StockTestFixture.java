0b97d1b5679db03e149765e1dfe57952e18f2a9f
Unagi-zoso <Unagi.zoso@gmail.com>
2025-02-15 21:05:05 +0900

    Fix 잘못된 Test Fixture 수정 (createTestStockInfoDtoList)
    TEST_COMPANY_CODE -> TEST_COMPANY_NAME

--- ktb-be-dev-assignment/src/test/java/org/ktb/ktbbedevassignment/fixture/StockTestFixture.java
+++ ktb-be-dev-assignment/src/test/java/org/ktb/ktbbedevassignment/fixture/StockTestFixture.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import org.ktb.ktbbedevassignment.dto.StockInfoDto;
-import static org.ktb.ktbbedevassignment.fixture.CompanyTestFixture.TEST_COMPANY_CODE;
+import static org.ktb.ktbbedevassignment.fixture.CompanyTestFixture.TEST_COMPANY_NAME;
 
 public class StockTestFixture {
 
@@ -31,7 +31,7 @@ public class StockTestFixture {
     public static List<StockInfoDto> createTestStockInfoDtoList(int size) {
         List<StockInfoDto> stockInfoDtoList = new ArrayList<>();
         for (int i = 0; i < size; i++) {
-            stockInfoDtoList.add(createTestStockInfoDto(TEST_COMPANY_CODE, plusDay(TEST_TRADE_DATE, i), TEST_CLOSING_PRICE));
+            stockInfoDtoList.add(createTestStockInfoDto(TEST_COMPANY_NAME, plusDay(TEST_TRADE_DATE, i), TEST_CLOSING_PRICE));
         }
         return stockInfoDtoList;
     }
