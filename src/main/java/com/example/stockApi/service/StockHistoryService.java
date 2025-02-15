ebec03bcf8627fbfdd4dd7e17e68f555e3d26a26
choiyoorim <yulim3839@naver.com>
2025-02-15 17:25:55 +0900

    feat: ✨ DB 연동 테스트를 위한 임시 서비스 구현

--- src/main/java/com/example/stockApi/service/StockHistoryService.java
+++ src/main/java/com/example/stockApi/service/StockHistoryService.java
@@ -1,4 +1,26 @@
 package com.example.stockApi.service;
 
+import com.example.stockApi.dto.stock.StockHistoryResponse;
+import com.example.stockApi.entity.StocksHistoryEntity;
+import com.example.stockApi.repository.stock.StocksHistoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
 public class StockHistoryService {
+
+    private final StocksHistoryRepository stocksHistoryRepository;
+
+    @Autowired
+    public StockHistoryService(StocksHistoryRepository stocksHistoryRepository) {
+        this.stocksHistoryRepository = stocksHistoryRepository;
+    }
+
+    public List<StocksHistoryEntity> getStockHistory(String companyCode, LocalDate startDate, LocalDate endDate){
+        return stocksHistoryRepository.findByCompanyCodeAndTradeDateBetween(companyCode, startDate, endDate);
+    }
+
 }
