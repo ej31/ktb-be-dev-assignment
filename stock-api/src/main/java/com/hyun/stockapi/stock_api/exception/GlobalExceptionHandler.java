6e549f5e3cc1b1fa71ec96c2667b1b6080cab249
hyun.lee <qwe7105c@naver.com>
2025-02-16 02:03:53 +0900

    fix : error 코드 수정 및 test코드 작성

--- stock-api/src/main/java/com/hyun/stockapi/stock_api/exception/GlobalExceptionHandler.java
+++ stock-api/src/main/java/com/hyun/stockapi/stock_api/exception/GlobalExceptionHandler.java
@@ -16,49 +16,57 @@ public class GlobalExceptionHandler {
     // API Key가 없는 경우 → 400
     @ExceptionHandler(ApiKeyMissingException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleMissingApiKey(ApiKeyMissingException ex) {
-        return ex.getMessage();
+    public ResponseEntity<Map<String,String>> handleMissingApiKey(ApiKeyMissingException ex) {
+        Map<String,String> errorResponse = new HashMap<>();
+        errorResponse.put("error",ex.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
     }
 
     // API Key가 잘못된 경우 → 403
     @ExceptionHandler(ApiKeyInvalidException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
-    public String handleInvalidApiKey(ApiKeyInvalidException ex) {
-        return ex.getMessage();
+    public ResponseEntity<Map<String,String>> handleInvalidApiKey(ApiKeyInvalidException ex) {
+        Map<String,String> errorResponse = new HashMap<>();
+        errorResponse.put("error",ex.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
     }
 
     // 요청 파라미터 잘못됨 → 400
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleIllegalArgument(IllegalArgumentException ex) {
-        return ex.getMessage();
+    public ResponseEntity<Map<String,String>> handleIllegalArgument(IllegalArgumentException ex) {
+        Map<String,String> errorResponse = new HashMap<>();
+        errorResponse.put("error",ex.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
     }
 
     //잘못된 날짜 형식 입력 ->400
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<Map<String,String>> handleTypeMismatch(MethodArgumentTypeMismatchException ex){
-        Map<String, String> error = new HashMap<>();
+        Map<String, String> errorResponse = new HashMap<>();
         if(ex.getRequiredType() !=null && ex.getRequiredType().equals(java.time.LocalDate.class)){
-            error.put("error","날짜 형식이 올바르지 않습니다. (yyyy-MM-dd 형식으로 입력해주세요)");
+            errorResponse.put("error","날짜 형식이 올바르지 않습니다. (yyyy-MM-dd 형식으로 입력해주세요)");
         } else{
-            error.put("error","잘못된 요청 형식 입니다.");
+            errorResponse.put("error","잘못된 요청 형식 입니다.");
         }
-        return ResponseEntity.badRequest().body(error);
+        return ResponseEntity.badRequest().body(errorResponse);
     }
 
     //존재하지 않는 요청 값 -> 400
     @ExceptionHandler(org.springframework.web.bind.MissingServletRequestParameterException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<Map<String, String>> handleMissingParams(org.springframework.web.bind.MissingServletRequestParameterException ex) {
-        Map<String, String> error = new HashMap<>();
-        error.put("error", ex.getParameterName() + " 파라미터가 누락되었습니다.");
-        return ResponseEntity.badRequest().body(error);
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", ex.getParameterName() + " 파라미터가 누락되었습니다.");
+        return ResponseEntity.badRequest().body(errorResponse);
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String handleException(Exception ex) {
-        return "서버 에러: " + ex.getMessage();
+    public ResponseEntity<Map<String, String>> handleException(Exception ex) {
+        Map<String,String> errorResponse = new HashMap<>();
+        errorResponse.put("error",ex.getMessage());
+        return ResponseEntity.badRequest().body(errorResponse);
     }
 }
---
+++ stock-api/src/test/java/com/hyun/stockapi/stock_api/controller/StockControllerTest.java
@@ -0,0 +1,44 @@
+package com.hyun.stockapi.stock_api.controller;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hyun.stockapi.stock_api.service.StockService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(StockController.class)
+class StockControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @MockitoBean
+    private StockService stockService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private static final String VALID_API_KEY = "c18aa07f-f005-4c2f-b6db-dff8294e6b5e";
+
+    @Test
+    @DisplayName("1) API Key 누락시 400 Bad Request")
+    void testMissingApiKey() throws Exception{
+
+        mockMvc.perform(get("/api/v1/stocks")
+                .param("companyCode","AAPL")
+                .param("tradeDate","2020-01-01"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.error").value("API Key가 누락되었습니다. (쿼리파라미터 apikey 또는 헤더 x-api-key 사용)"));
+    }
+
+}
