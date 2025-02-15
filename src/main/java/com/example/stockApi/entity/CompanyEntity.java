7b01dc5feab3385c47ff4b7427008665bf5c6c57
choiyoorim <yulim3839@naver.com>
2025-02-16 01:57:55 +0900

    fix: 🔨 기본키 중복 추가 오류 수정

--- src/main/java/com/example/stockApi/entity/CompanyEntity.java
+++ src/main/java/com/example/stockApi/entity/CompanyEntity.java
@@ -13,12 +13,10 @@ import lombok.Setter;
 @NoArgsConstructor
 @Table(name = "company", schema = "assignment")
 public class CompanyEntity {
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     @Column(name = "company_code", nullable = false, length = 10)
     private String companyCode;
 
-    @Id
     @Column(name = "company_name", nullable = false)
     private String companyName;
 }
