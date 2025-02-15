ca7a44cc1a99233a2a9ab7838f51ed97241211fc
ghkddlscks19 <ghkddlscks19@naver.com>
2025-02-15 23:19:25 +0900

    feat: 사용자의 입력 값에 맞게 주식 정보 가져오는 로직 추가

---
+++ src/main/java/org/ktb/stock/service/StockService.java
@@ -0,0 +1,21 @@
+package org.ktb.stock.service;
+
+import lombok.RequiredArgsConstructor;
+import org.ktb.stock.dto.StockResponseDto;
+import org.ktb.stock.dto.StockSearchDto;
+import org.ktb.stock.repository.StockRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class StockService {
+    private final StockRepository stockRepository;
+
+    public List<StockResponseDto> getStocks(StockSearchDto stockSearchDto) {
+        return stockRepository.findStocks(stockSearchDto);
+    }
+}
