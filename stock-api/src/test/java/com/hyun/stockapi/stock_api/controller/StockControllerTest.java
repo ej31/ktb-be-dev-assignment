60001bc5dd995b15a6a34aa06dafdfe68f4a417d
hyun.lee <qwe7105c@naver.com>
2025-02-16 02:32:09 +0900

    feat : controller test코드 작성

--- stock-api/src/test/java/com/hyun/stockapi/stock_api/controller/StockControllerTest.java
+++ stock-api/src/test/java/com/hyun/stockapi/stock_api/controller/StockControllerTest.java
@@ -10,11 +10,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(StockController.class)
 class StockControllerTest {
@@ -52,5 +50,39 @@ class StockControllerTest {
                 .andExpect(status().isForbidden())
                 .andExpect(jsonPath("$.error").value("유효하지 않은 API Key입니다."));
     }
+    @Test
+    @DisplayName("3) 필수 파라 미터 누락: companyCode 없음 400 Bad Request")
+    void testMissingCompanyCode() throws Exception{
+
+        mockMvc.perform(get("/api/v1/stocks")
+                        .param("tradeDate", "2020-01-01")
+                        .param("apikey", VALID_API_KEY))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.error").value("companyCode 파라미터가 누락되었습니다."));
+    }
+
+    @Test
+    @DisplayName("4) 날짜 형식이 올바르지 않은 경우 400 Bad Request")
+    void testInvalidDateFormat() throws Exception{
+
+        mockMvc.perform(get("/api/v1/stocks")
+                .param("companyCode","AAPL")
+                .param("tradeDate","202001-01")
+                .param("apikey",VALID_API_KEY))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.error").value("날짜 형식이 올바르지 않습니다. (yyyy-MM-dd 형식으로 입력해주세요)"));
+    }
+    @Test
+    @DisplayName("5) 정상 호출 => 200 OK")
+    void testValidRequest() throws Exception {
+
+        mockMvc.perform(get("/api/v1/stocks")
+                        .param("companyCode", "AAPL")
+                        .param("tradeDate", "2025-02-15")
+                        .param("apikey", VALID_API_KEY))
+                .andExpect(status().isOk())
+                .andExpect(content().string("[]"));
+    }
+
 
 }
