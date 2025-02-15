8ff774232eec00db8ca24a04e132a63ec6cc5deb
Unagi-zoso <Unagi.zoso@gmail.com>
2025-02-15 14:32:02 +0900

    Refactor application 확장자 변경 및 프로젝트 포매팅 적용
    yml 을 선호하여 변경합니다. 그 외 전체 파일에 Intellj 기본 포매팅을 적용했습니다.

--- ktb-be-dev-assignment/src/main/java/org/ktb/ktbbedevassignment/KtbBeDevAssignmentApplication.java
+++ ktb-be-dev-assignment/src/main/java/org/ktb/ktbbedevassignment/KtbBeDevAssignmentApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KtbBeDevAssignmentApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KtbBeDevAssignmentApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KtbBeDevAssignmentApplication.class, args);
+    }
 
 }
--- ktb-be-dev-assignment/src/test/java/org/ktb/ktbbedevassignment/KtbBeDevAssignmentApplicationTests.java
+++ ktb-be-dev-assignment/src/test/java/org/ktb/ktbbedevassignment/KtbBeDevAssignmentApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class KtbBeDevAssignmentApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
