db637d42bf4d3a391871fb9b35396975bc987378
sanghyuk Kim <tkdgur5031@naver.com>
2025-02-15 19:42:22 +0900

    feat: 응답 형식 통일 - 성공 응답 : SuccessResponse - 실패 응답 : ErrorResponse

--- src/main/java/org/ktb/dev/assignment/core/response/SuccessResponse.java
+++ src/main/java/org/ktb/dev/assignment/core/response/SuccessResponse.java
@@ -1,4 +1,37 @@
 package org.ktb.dev.assignment.core.response;
 
-public class SuccessResponse {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+@Schema(description = "성공 Response")
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SuccessResponse<T> {
+
+    @Schema(description = "성공 여부", defaultValue = "true")
+    private final boolean success = true;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T data;
+
+    public static <T> SuccessResponse<T> of(T data) {
+        SuccessResponse<T> successResponse = new SuccessResponse<>();
+
+        successResponse.data = data;
+
+        return successResponse;
+    }
+
+    public static SuccessResponse<Void> ofNoData() {
+        return new SuccessResponse<>();
+    }
+
+    public ResponseEntity<SuccessResponse<T>> asHttp(HttpStatus httpStatus) {
+        return ResponseEntity.status(httpStatus).body(this);
+    }
 }
