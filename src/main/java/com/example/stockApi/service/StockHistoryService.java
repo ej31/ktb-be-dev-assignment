12a45967655f38a170aff5d49536ece78df8aacc
choiyoorim <yulim3839@naver.com>
2025-02-16 01:54:21 +0900

    feat: ✨ 주식 정보 조회 서비스 레이어 예외처리 추가

--- src/main/java/com/example/stockApi/service/StockHistoryService.java
+++ src/main/java/com/example/stockApi/service/StockHistoryService.java
@@ -2,7 +2,10 @@ package com.example.stockApi.service;
 
 import com.example.stockApi.dto.stock.StockHistoryResponse;
 import com.example.stockApi.entity.StocksHistoryEntity;
+import com.example.stockApi.exception.ApiException;
+import com.example.stockApi.repository.stock.CompanyRepository;
 import com.example.stockApi.repository.stock.StocksHistoryRepository;
+import com.example.stockApi.exception.ErrorCode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,14 +16,23 @@ import java.util.List;
 public class StockHistoryService {
 
     private final StocksHistoryRepository stocksHistoryRepository;
+    private final CompanyRepository companyRepository;
 
     @Autowired
-    public StockHistoryService(StocksHistoryRepository stocksHistoryRepository) {
+    public StockHistoryService(StocksHistoryRepository stocksHistoryRepository, CompanyRepository companyRepository) {
         this.stocksHistoryRepository = stocksHistoryRepository;
+        this.companyRepository = companyRepository;
     }
 
-    public List<StocksHistoryEntity> getStockHistory(String companyCode, LocalDate startDate, LocalDate endDate){
-        return stocksHistoryRepository.findByCompanyCodeAndTradeDateBetween(companyCode, startDate, endDate);
+    public List<StockHistoryResponse> getStockHistory(String companyCode, LocalDate startDate, LocalDate endDate){
+        boolean exists = companyRepository.findByCompanyCode(companyCode).isPresent();
+        if(!exists){
+            throw new ApiException(ErrorCode.COMPANY_NOT_FOUND);
+        }
+
+        List<StockHistoryResponse> stockHistoryEntities = stocksHistoryRepository.findStockHistoryWithCompanyName(companyCode, startDate, endDate);
+
+        return stockHistoryEntities;
     }
 
 }
