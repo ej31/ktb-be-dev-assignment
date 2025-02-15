e4c7f8b166272ba205c8b59c27174709c76a74de
choiyoorim <yulim3839@naver.com>
2025-02-16 01:54:51 +0900

    feat: ✨ 주식 정보 기반 기업명 검색을 위한 조인 쿼리 도입

--- src/main/java/com/example/stockApi/repository/stock/StocksHistoryRepository.java
+++ src/main/java/com/example/stockApi/repository/stock/StocksHistoryRepository.java
@@ -1,8 +1,11 @@
 package com.example.stockApi.repository.stock;
 
+import com.example.stockApi.dto.stock.StockHistoryResponse;
 import com.example.stockApi.entity.StocksHistoryEntity;
 import com.example.stockApi.entity.StocksHistoryEntityPK;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -10,5 +13,18 @@ import java.util.List;
 
 @Repository
 public interface StocksHistoryRepository extends JpaRepository<StocksHistoryEntity, StocksHistoryEntityPK> {
-    List<StocksHistoryEntity> findByCompanyCodeAndTradeDateBetween(String companyCode, LocalDate startDate, LocalDate endDate);
+    @Query("""
+        SELECT new com.example.stockApi.dto.stock.StockHistoryResponse(
+            c.companyName, s.tradeDate, s.closePrice
+        )
+        FROM StocksHistoryEntity s
+        JOIN CompanyEntity c ON s.companyCode = c.companyCode
+        WHERE s.companyCode = :companyCode
+        AND s.tradeDate BETWEEN :startDate AND :endDate
+    """)
+    List<StockHistoryResponse> findStockHistoryWithCompanyName(
+            @Param("companyCode") String companyCode,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate
+    );
 }
