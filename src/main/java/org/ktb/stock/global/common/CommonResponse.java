e903c8cfce8004217b3c0e3e0b58e678a98df854
ghkddlscks19 <ghkddlscks19@naver.com>
2025-02-16 17:30:00 +0900

    refactor: ApiResponse -> CommonResponse 클래스 명 변경

--- src/main/java/org/ktb/stock/global/common/CommonResponse.java
+++ src/main/java/org/ktb/stock/global/common/CommonResponse.java
@@ -2,7 +2,6 @@ package org.ktb.stock.global.common;
 
 import lombok.*;
 import org.ktb.stock.global.error.code.ErrorCode;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
@@ -11,16 +10,16 @@ import java.util.List;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ApiResponse<T>{
+public class CommonResponse<T>{
     private boolean success; // 요청의 성공 여부를 나타내기 위함
     private String message; // 사용자나 개발자가 확인할 수 있는 설명을 제공하기 위함
     private T data; // 데이터를 실제로 담는 필드
 
-    public static <T> ResponseEntity<ApiResponse<T>> success(T data) {
+    public static <T> ResponseEntity<CommonResponse<T>> success(T data) {
         String message = (data instanceof List && ((List<?>) data).isEmpty())
                 ? "해당 기간에 조회된 주식 데이터가 없습니다."
                 : "주식 데이터를 성공적으로 조회했습니다.";
-        ApiResponse<T> response = ApiResponse.<T>builder()
+        CommonResponse<T> response = CommonResponse.<T>builder()
                 .success(true)
                 .message(message)
                 .data(data)
@@ -28,8 +27,8 @@ public class ApiResponse<T>{
         return ResponseEntity.ok(response);
     }
 
-    public static <T> ResponseEntity<ApiResponse<T>> error(ErrorCode errorCode) {
-        ApiResponse<T> response = ApiResponse.<T>builder()
+    public static <T> ResponseEntity<CommonResponse<T>> error(ErrorCode errorCode) {
+        CommonResponse<T> response = CommonResponse.<T>builder()
                 .success(false)
                 .message(errorCode.getMessage())
                 .build();
