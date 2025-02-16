371995d6cb688a9ecb52365c9a3a91a809c998f2
groom <hayaon0603@naver.com>
2025-02-17 02:38:54 +0900

    Feat: API Key 인증 인터셉터 및 WebConfig 추가

---
+++ src/main/java/com/example/stock/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.example.stock.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final ApiKeyInterceptor apiKeyInterceptor;
+
+    @Autowired
+    public WebConfig(ApiKeyInterceptor apiKeyInterceptor) {
+        this.apiKeyInterceptor = apiKeyInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(apiKeyInterceptor)              // API Key 검증
+                .addPathPatterns("/api/v1/**");                // /api/v1으로 시작하는 모든 엔드포인트
+    }
+}
