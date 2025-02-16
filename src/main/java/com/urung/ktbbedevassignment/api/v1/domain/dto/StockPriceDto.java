a3dc420f6afa38afb8865caa4c1d28a654b31323
uhyunglee <uhyunglee@naver.com>
2025-02-17 07:52:02 +0900

    [Feat] get Stock Price API

--- src/main/java/com/urung/ktbbedevassignment/api/v1/domain/dto/StockPriceDto.java
+++ src/main/java/com/urung/ktbbedevassignment/api/v1/domain/dto/StockPriceDto.java
@@ -1,4 +1,39 @@
 package com.urung.ktbbedevassignment.api.v1.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class StockPriceDto {
+
+    @Data
+    @NoArgsConstructor
+    public static class StockPriceRequest{
+        private String companyCode;
+        @DateTimeFormat(pattern = "yyyy-MM-dd")
+        private LocalDate startDate;
+        @DateTimeFormat(pattern = "yyyy-MM-dd")
+        private LocalDate endDate;
+
+        @Builder
+        public StockPriceRequest(String companyCode, LocalDate startDate, LocalDate endDate) {
+            this.companyCode = companyCode;
+            this.startDate = startDate;
+            this.endDate = endDate;
+        }
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class StockPriceResponse{
+        private String companyName;
+        private String tradeDate;
+        private float closingPrice;
+    }
 }
--- src/main/java/com/urung/ktbbedevassignment/domain/stocksHistory/repository/StocksHistoryRepository.java
+++ src/main/java/com/urung/ktbbedevassignment/domain/stocksHistory/repository/StocksHistoryRepository.java
@@ -1,7 +1,27 @@
 package com.urung.ktbbedevassignment.domain.stocksHistory.repository;
 
+import com.urung.ktbbedevassignment.api.v1.domain.dto.StockPriceDto;
 import com.urung.ktbbedevassignment.domain.stocksHistory.entity.StocksHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface StocksHistoryRepository extends JpaRepository<StocksHistory, String> {
+    List<StocksHistory> findByCompanyCodeAndTradeDateBetween(
+            String companyCode,
+            LocalDate startDate,
+            LocalDate endDate
+    );
+    @Query("SELECT sh FROM StocksHistory sh " +
+            "WHERE sh.companyCode = :companyCode " +
+            "AND sh.tradeDate BETWEEN :startDate AND :endDate " +
+            "ORDER BY sh.tradeDate")
+    List<StocksHistory> findStockHistories(
+            @Param("companyCode") String companyCode,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate
+    );
 }
