30baa88eb02c570c73af595896d677e77b90ad7f
Unagi-zoso <Unagi.zoso@gmail.com>
2025-02-17 01:46:12 +0900

    Feat Api 의 상수값 일부를 파일로 관리
    메인에서 IDE 자동완성이나 성능상 이유로 관리합니다.

---
+++ ktb-be-dev-assignment/src/main/java/org/ktb/ktbbedevassignment/constant/ApiConstant.java
@@ -0,0 +1,8 @@
+package org.ktb.ktbbedevassignment.constant;
+
+public class ApiConstant {
+
+    public static final String API_KEY_HEADER = "x-api-key";
+
+    public static final String API_KEY_PARAM = "apikey";
+}
