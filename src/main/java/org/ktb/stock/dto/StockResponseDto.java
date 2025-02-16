e5af0a33131f6a2434af32d1411869540b073eb6
ghkddlscks19 <ghkddlscks19@naver.com>
2025-02-16 16:13:15 +0900

    fix: Date -> LocalDate

--- src/main/java/org/ktb/stock/dto/StockResponseDto.java
+++ src/main/java/org/ktb/stock/dto/StockResponseDto.java
@@ -3,12 +3,12 @@ package org.ktb.stock.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 @Getter
 @AllArgsConstructor
 public class StockResponseDto {
     private String companyName;
-    private Date tradeDate;
+    private LocalDate tradeDate;
     private float closingPrice;
 }
