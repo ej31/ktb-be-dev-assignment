cc2515b1dd052e8a50564081b39619e554d3db45
jenny.kang <deco1202@naver.com>
2025-02-17 00:56:25 +0900

    refactor: .env 추가 및 적용

--- src/main/java/com/jenny/ktb_be_dev_assignment/KtbBeDevAssignmentApplication.java
+++ src/main/java/com/jenny/ktb_be_dev_assignment/KtbBeDevAssignmentApplication.java
@@ -1,10 +1,18 @@
 package com.jenny.ktb_be_dev_assignment;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KtbBeDevAssignmentApplication {
+
+	static {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(KtbBeDevAssignmentApplication.class, args);
