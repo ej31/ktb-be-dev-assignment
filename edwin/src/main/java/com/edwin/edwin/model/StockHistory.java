8f94f06454995f8545652fc531909a4f9fc38501
gkwon <gkwon@student.42seoul.kr>
2025-02-17 03:47:30 +0900

    feat: Add StockHistory entity model for database mapping

---
+++ edwin/src/main/java/com/edwin/edwin/model/StockHistory.java
@@ -0,0 +1,27 @@
+package com.edwin.edwin.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class StockHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "company_code")
+    private String companyCode;
+    @Column(name = "trade_date")
+    private LocalDate tradeDate;
+    @Column(name = "close_price")
+    private float closePrice;
+}
