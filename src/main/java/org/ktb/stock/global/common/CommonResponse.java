2da10ec51e1a577fa999521e75383dc81c753295
ghkddlscks19 <ghkddlscks19@naver.com>
2025-02-16 21:01:35 +0900

    feat: JAXB 어노테이션 및 xml 반환 추가

--- src/main/java/org/ktb/stock/global/common/CommonResponse.java
+++ src/main/java/org/ktb/stock/global/common/CommonResponse.java
@@ -1,12 +1,18 @@
 package org.ktb.stock.global.common;
 
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlRootElement;
 import lombok.*;
 import org.ktb.stock.global.error.code.ErrorCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 @Getter
+@XmlRootElement(name = "CommonResponse")
+@XmlAccessorType(XmlAccessType.FIELD)
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -15,7 +21,7 @@ public class CommonResponse<T>{
     private String message; // 사용자나 개발자가 확인할 수 있는 설명을 제공하기 위함
     private T data; // 데이터를 실제로 담는 필드
 
-    public static <T> ResponseEntity<CommonResponse<T>> success(T data) {
+    public static <T> ResponseEntity<CommonResponse<T>> success(T data, MediaType mediaType) {
         String message = (data instanceof List && ((List<?>) data).isEmpty())
                 ? "해당 기간에 조회된 주식 데이터가 없습니다."
                 : "주식 데이터를 성공적으로 조회했습니다.";
@@ -24,14 +30,18 @@ public class CommonResponse<T>{
                 .message(message)
                 .data(data)
                 .build();
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok()
+                .contentType(mediaType)
+                .body(response);
     }
 
-    public static <T> ResponseEntity<CommonResponse<T>> error(ErrorCode errorCode) {
+    public static <T> ResponseEntity<CommonResponse<T>> error(ErrorCode errorCode, MediaType mediaType) {
         CommonResponse<T> response = CommonResponse.<T>builder()
                 .success(false)
                 .message(errorCode.getMessage())
                 .build();
-        return ResponseEntity.status(errorCode.getStatus()).body(response);
+        return ResponseEntity.status(errorCode.getStatus())
+                .contentType(mediaType)
+                .body(response);
     }
 }
