dcc4e39f9ffab72e3a9f3c5e3d37184a04499afe
ghkddlscks19 <ghkddlscks19@naver.com>
2025-02-16 16:07:02 +0900

    feat: Swagger 설정 추가

--- src/main/java/org/ktb/stock/config/SwaggerConfig.java
+++ src/main/java/org/ktb/stock/config/SwaggerConfig.java
@@ -1,4 +1,18 @@
 package org.ktb.stock.config;
 
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "주식 정보 조회 API",
+                description = "주식 정보를 조회하는 API 입니다.",
+                version = "v1.0"
+        )
+)
+@Configuration
 public class SwaggerConfig {
 }
