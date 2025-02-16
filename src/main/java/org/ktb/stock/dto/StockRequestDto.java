832b2ca8fab2b7375b40e647f38dcd8d89991bc2
ghkddlscks19 <ghkddlscks19@naver.com>
2025-02-16 17:47:56 +0900

    feat: Swagger 및 날짜 형식 추가

--- src/main/java/org/ktb/stock/dto/StockRequestDto.java
+++ src/main/java/org/ktb/stock/dto/StockRequestDto.java
@@ -1,23 +1,28 @@
 package org.ktb.stock.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Schema(description = "주식 조회 요청 DTO")
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class StockRequestDto {
+    @Schema(description = "회사 코드", example = "AAPL")
     @JsonProperty("company_code")
     private String companyCode;
 
+    @Schema(description = "조회 시작 날짜", example = "2020-01-01")
     @JsonProperty("start_date")
-    @JsonFormat(pattern = "yyyy-MM-dd")
+    @Pattern(regexp = "\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01])")
     private String startDate;
 
+    @Schema(description = "조회 종료 날짜", example = "2020-01-31")
     @JsonProperty("end_date")
-    @JsonFormat(pattern = "yyyy-MM-dd")
+    @Pattern(regexp = "\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01])")
     private String endDate;
 }
