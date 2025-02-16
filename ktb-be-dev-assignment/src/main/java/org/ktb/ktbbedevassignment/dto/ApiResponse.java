8354999065eb1979faa6d5416c789a19eddc0713
Unagi-zoso <Unagi.zoso@gmail.com>
2025-02-17 07:27:20 +0900

    Comment 특정 응답 값을 설계한 이유 코멘트
    내용 상세에 특정 응답 값을 설계한 이유를 주석으로 달아란 요구가 있어 추가했습니다.
    
    응답 값의 뜻이 ApiResponse 같은 응답 포맷을 의미하는 것인지 아니면 API 별 상태번호 선정 이유를 의미하는지 물어봤을 것 같은데 시간이 부족하여 그러지 못하였다.

--- ktb-be-dev-assignment/src/main/java/org/ktb/ktbbedevassignment/dto/ApiResponse.java
+++ ktb-be-dev-assignment/src/main/java/org/ktb/ktbbedevassignment/dto/ApiResponse.java
@@ -5,6 +5,19 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import java.time.LocalDateTime;
 import org.springframework.http.HttpStatus;
 
+/*
+    특정 응답 값을 설계한 이유
+    일관된 특정 으답 값을 가짐으로 사용자에게 예측을 가능하게 도울 수 있습니다.
+    사용자의 코드도 일관적으로 관리할 수 있습니다.
+    오류 처리 시에도 중앙에서 일관적으로 처리할 수 있습니다.
+
+    * API 응답을 담는 DTO 클래스
+    * 성공, 실패 시 공통적인 형태를 가지기 위해 ApiResponse 클래스를 사용
+    * timestamp 를 통해 응답 시간을 기록
+    * status 를 통해 HTTP 상태 코드를 기록
+    * message 를 통해 응답 메시지를 기록
+    * data 를 통해 응답 데이터를 기록 (성공 시 데이터가 있거나 빈 배열이며, 실패 시 반환 결과에서 제외될 수 있습니다.)
+ */
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record ApiResponse<T>(
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
