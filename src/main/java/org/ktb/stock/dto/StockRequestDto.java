48ba741acbe6535845467a6df1aa2cddd3ec59e3
ghkddlscks19 <ghkddlscks19@naver.com>
2025-02-16 16:12:59 +0900

    feat: @JsonFormat 추가

--- src/main/java/org/ktb/stock/dto/StockRequestDto.java
+++ src/main/java/org/ktb/stock/dto/StockRequestDto.java
@@ -1,5 +1,6 @@
 package org.ktb.stock.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,8 +14,10 @@ public class StockRequestDto {
     private String companyCode;
 
     @JsonProperty("start_date")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private String startDate;
 
     @JsonProperty("end_date")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private String endDate;
 }
