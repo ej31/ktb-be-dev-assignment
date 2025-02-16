ad0386250b5b2b23cc191d2815d76a1d2b69ccd8
ghkddlscks19 <ghkddlscks19@naver.com>
2025-02-16 17:46:19 +0900

    refactor: stockSearchDto -> stockServiceDto 클래스 명 변경

--- src/main/java/org/ktb/stock/dto/StockServiceDto.java
+++ src/main/java/org/ktb/stock/dto/StockServiceDto.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 
 @Getter
 @AllArgsConstructor
-public class StockSearchDto {
+public class StockServiceDto {
     private String companyCode;
     private LocalDate startDate;
     private LocalDate endDate;
